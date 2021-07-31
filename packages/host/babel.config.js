d138de1bbafda7d0162f64df5d2e62a597c4f03b
hoondeveloper <hoonskyn@cau.ac.kr>
2021-07-31 18:14:00 +0900

    feat(host): babel.config, webpack.config

---
+++ packages/host/babel.config.js
@@ -0,0 +1,39 @@
+module.exports = function config(api) {
+  return {
+    presets: ["@babel/preset-env"],
+
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+      ["@babel/plugin-transform-react-jsx", {
+        "runtime": "automatic"
+      }]
+    ],
+
+    overrides: [
+      {
+        test: /\.tsx?$/,
+        presets: ["@babel/preset-typescript"],
+      },
+      {
+        test: /\.tsx$/,
+        presets: [
+          [
+            "@babel/preset-react",
+            {
+              development: api.env() === "development",
+              useBuiltIns: true,
+              runtime: "automatic",
+              importSource: "@emotion/react",
+            },
+          ],
+        ],
+        plugins: ["@emotion/babel-plugin"],
+      },
+      {
+        test: "**/*.d.ts",
+        presets: [["@babel/preset-env", { targets: { esmodules: true } }]],
+      },
+    ],
+  };
+};
