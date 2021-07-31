618fca55c42c420bf14eb4d05851d60f262faf86
hoondeveloper <hoonskyn@cau.ac.kr>
2021-07-31 19:12:26 +0900

    feat(host): federate remote apps into host

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import * as React from 'react';
+
+const Remote1 = React.lazy(() => import('remote1/App'));
+const Remote2 = React.lazy(() => import('remote2/App'));
 
 const App: React.FC = () => {
-  return <>Hi</>;
+  return (
+    <>
+      <React.Suspense fallback={<>Loading Remote1</>}>
+        <Remote1 />
+      </React.Suspense>
+      <React.Suspense fallback={<>Loading Remote2</>}>
+        <Remote2 />
+      </React.Suspense>
+    </>
+  );
 };
 
 export default App;
